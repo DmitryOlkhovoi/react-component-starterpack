ec5d6896fd3a97184b645635e755d41239a51606
Dmitry Olkhovoi <auuine@gmail.com>
2017-08-29 12:37:00 +0300

    Add some files

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './styles/index.css'
+
+class MyComponent extends Component {
+  render() {
+    return (
+      <div styleName="my-component">
+        <p>The best component ever</p>
+      </div>
+    )
+  }
+}
+
+export default MyComponent
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader?importLoader=1&modules&localIdentName=[path]___[name]__[local]___[hash:base64:5]" }
+        ]
+      }
+    ]
+  }
+}
